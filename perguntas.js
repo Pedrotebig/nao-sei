a921c863f37870e886aaa533dacd0206df530e87
Pedrotebig <140452400+Pedrotebig@users.noreply.github.com>
2024-10-11 09:35:06 -0300

    \zxcvbn

---
+++ perguntas.js
@@ -0,0 +1,121 @@
+criaCartão(
+    'Matemática',
+    'A afirmação: Tudo é matemática. está correta?',
+    'Não, os seres humanos traduzem coisas para a Matemática'
+)
+
+criaCartão(
+    'Química',
+    'O que são cadeias de carbono?',
+    'São estruturas moleculares que possuem principalmente carbonos'
+)
+
+criaCartão(
+    'Biologia',
+    'Qual o maior osso do ser Humano?',
+    'Fêmur'
+)
+
+criaCartão(
+    'Projeto de vida',
+    'O que o Super Homem queria ser',
+    'Um herói ou uma pessoa comum'
+)
+
+criaCartão(
+    'Educação financeira',
+    'É itereçante investir em um país?',
+    'Sim, se o país for estável'
+)
+
+criaCartão(
+    'Português',
+    'Onde se usa o texto dissertativo argumentativo?',
+    'ENEM'
+)
+
+criaCartão(
+    'Sociologia',
+    'uma comunidade é um espaço social pobre?',
+    'Não, existem vários tipos de comunidades'
+)
+
+criaCartão(
+    'Química',
+    'Qual é a fórmula da água?',
+    'H₂O',
+)
+
+criaCartão(
+    'História',
+    'Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis'
+)
+
+criaCartão(
+    'Geografia',
+    'Qual é a Capital da França?',
+    'Paris'
+)
+
+criaCartão(
+    'Biologia',
+    'O que é uma célula?',
+    'A unidade básica da vida'
+)
+
+criaCartão(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Álvarez Cabral'
+)
+
+criaCartão(
+    'Filosofia',
+    'O que estuda a História?',
+    'Acontecimentos do passado'
+)
+
+criaCartão(
+    'Biologia',
+    'Qual é a principal função do sistema circulatório?',
+    'Transportar sangue'
+)
+
+criaCartão(
+    'Ciência',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartão(
+    'Português',
+    'O que é um adjetivo?',
+    'Uma palavra que descreve um substantivo'
+)
+
+criaCartão(
+    'Biologia',
+    'O que é fotossíntese?',
+    'Processo que as plantas usam para produzir alimento.'
+)
+
+criaCartão(
+    'Artes',
+    'Quem pintou "A Última Ceia"?',
+    'Leornado Da Vinci'
+)
+
+criaCartão(
+    'Matemática',
+    'Qual é a fórmula do cálculo da área de um triângulo?',
+    '(base x altura) / 2'
+)
+
+criaCartão(
+    'Matemática',
+    'Pergunta',
+    'Resposta'
+)
+
+//Ao todo são 20 perguntas de memorização do ensino médio
